adb9b9fa9c8ebc3fbe676ee5b328ca3d66c4af50
Vakon <jamespagedev@outlook.com>
2019-08-10 10:21:31 -0700

    contact send functionality complete

--- src/components/Modals/ContactSendSuccess.js
+++ src/components/Modals/ContactSendSuccess.js
@@ -4,21 +4,60 @@ import styled from 'styled-components';
 //============================================ styles =============================================
 const DivWrapper = styled.div`
   display: ${props => props.success === 'true' ? 'flex' : 'none'};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   top: 12rem;
   right: 0;
   left: 0;
   bottom: 0;
-  width: 800px;
-  height: 400px;
-  background: green;
+  width: 100%;
+  height: 30%;
+  z-index: 4005;
 `;
+
+const DivMessage = styled.div`
+  width: 22%;
+  background: white;
+`;
+
+const H1Message = styled.h1`
+  width: 100%;
+  padding-top: 3rem;
+  text-align: center;
+  font-size: 7rem;
+`;
+
+const DivButton = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin: 11rem 0;
+`;
+
+const ButtonOkay = styled.button`
+  display: block;
+  width: 35%;
+  text-align: center;
+  font-size: 4rem;
+  padding: 2rem 0;
+  border-radius: 2.5rem;
+  outline: none;
+  user-select: none;
+`
 
 //=========================================== component ===========================================
 const ContactSendSuccess = props => {
   return (
     <DivWrapper success={props.success.toString()}>
-      <h1>This is the ContactSendSuccess component</h1>
+      <DivMessage>
+        <H1Message>message sent successful</H1Message>
+        <DivButton>
+          <ButtonOkay onClick={ev => props.contactSuccessModalButton(ev)}>Okay</ButtonOkay>
+        </DivButton>
+      </DivMessage>
     </DivWrapper>
   );
 };
