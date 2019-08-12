4f01469aab4a6c40028c20acfff8d753b9ad75e9
Vakon <jamespagedev@outlook.com>
2019-08-12 16:29:26 -0700

    updated resume's

--- src/components/Resumes/Resumes.js
+++ src/components/Resumes/Resumes.js
@@ -11,27 +11,27 @@ import Resume from './Resume.js';
 const Data = [
   {
     resumeTitle: "Full Stack Dev",
-    resumeLink: "https://drive.google.com/file/d/18WVpEbOGspnGwwNXCkvC94iekShFIDhz/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1NhYE7X4DKb6aKQ6w-aKVWce82s1InQJ8/view?usp=sharing",
   },
   {
     resumeTitle: "Software Dev",
-    resumeLink: "https://drive.google.com/file/d/1ueYLYSHeW6jvfcXVtMNJKntYDxFoqTZ7/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1yz0-yxCSYEUX5lCv_dl8-KBFF0Dx5rPJ/view?usp=sharing",
   },
   {
     resumeTitle: "Front-End Dev",
-    resumeLink: "https://drive.google.com/file/d/1dSPD8WwO2nEYHY_yMpCvCokBcDwqDVSr/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1Znx9ML-O22EtjXDRdpbjGNVtpALgjoRA/view?usp=sharing",
   },
   {
     resumeTitle: "Backend-End Dev",
-    resumeLink: "https://drive.google.com/file/d/1spnELVYF5o9kxpHw6YZ6rfIbXUktvi0D/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1QxNGg07V9NTNhtydRz0JWlrsuhcTLqA_/view?usp=sharing",
   },
   {
     resumeTitle: "Test Engineer",
-    resumeLink: "https://drive.google.com/file/d/1W1s_5ig_oaombliBUqZKvYe6F4xTElZ-/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1-mTmG1OuabB674u3vU-Xt-tunXYN-CVT/view?usp=sharing",
   },
   {
     resumeTitle: "IT Admin",
-    resumeLink: "https://drive.google.com/file/d/1Qk5eoTx-lqZ330oMHiFpUnIqpN00OKGD/view?usp=sharing",
+    resumeLink: "https://drive.google.com/file/d/1BbfN8caw8aEiVvEOD-h4quuOkJW08wTA/view?usp=sharing",
   }
 ]
 
